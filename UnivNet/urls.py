48271fc15a06391084ab9050986c50d5839a1a40
Sayan Goswami <Sayan98@users.noreply.github.com>
2017-06-07 22:07:07 +0530

    Implement Google OAuth social login
    Implemented Google OAuth social login

--- UnivNet/urls.py
+++ UnivNet/urls.py
@@ -1,22 +1,15 @@
 """UnivNet URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-import contact
+from django.contrib.auth import views as auth_views
+from UnivNet import views as core_views
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^admin/', admin.site.urls),
     url(r'^contact/', include('contact.urls')),
 ]
